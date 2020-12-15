db57254dce92b990e40ece3d1da20b64e6b0426e
Adriane <rodrigues.adriane@outlook.com>
2020-12-15 19:27:37 -0300

    Primeiro commit

---
+++ src/main/java/com/br/adriane/guiabolso/mocktransacoes/MockTransacoesApplication.java
@@ -0,0 +1,13 @@
+package com.br.adriane.guiabolso.mocktransacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockTransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockTransacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/adriane/guiabolso/mocktransacoes/MockTransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.adriane.guiabolso.mocktransacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockTransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
